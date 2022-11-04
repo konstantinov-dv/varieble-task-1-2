796680b61b8fc6b9e58501b226e21f368eeb5365
Дима <konstantinov.dima.v@gmail.com>
2022-11-04 16:14:14 +0700

    Домашнее задание 1-2

---
+++ varieble/src/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+    public static void main(String[] args) {
+        //домашняя работа 1
+        //задание 1
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        //задание 2
+        dog = dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        //задание 3
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        // задание 4
+        var friend = 19;
+        System.out.println(friend);
+        friend = (friend + 2) / 7;
+        System.out.println(friend);
+
+        //задание 5
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10 / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+
+
+
+
+
+
+    }
+}
